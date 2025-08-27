fc1b4e35f3f62b38d21ad55f108d9d0eb2837636
PejuangCS <ilhamalki33@gmail.com>
2025-08-27 14:28:48 +0700

    apala

--- football_news/settings.py
+++ football_news/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-*)h+l5@e18p#r&yxydlt@g9j7ie=kn7ojae)jbfva5%dwfbasz
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","ilham-afuw-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
